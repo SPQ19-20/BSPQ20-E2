9dd172d6134cbd8a7a365c767edc6b354f83fa11
DonBusto <33113637+DonBusto@users.noreply.github.com>
2020-04-01 12:47:19 +0200

    login and payment windows w/o listeners

--- Netflix/src/main/java/BSPQ20_E2/Netflix/App.java
+++ Netflix/src/main/java/BSPQ20_E2/Netflix/App.java
@@ -1,13 +1,14 @@
 package BSPQ20_E2.Netflix;
 
+import BSPQ20_E2.Netflix.gui.Login;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		Login l = new Login();
+		l.setVisible(true);
+	}
 }
---
+++ Netflix/src/main/java/BSPQ20_E2/Netflix/gui/Login.java
@@ -0,0 +1,54 @@
+package BSPQ20_E2.Netflix.gui;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Login extends JFrame {
+	private JTextField tfUser;
+	private JTextField tfPasswd;
+	public Login() {
+		setSize(459,248);
+		setTitle("Netflix - Login");
+		setResizable(false);
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 126, 434, 83);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblUser = new JLabel("User:");
+		lblUser.setBounds(10, 0, 119, 36);
+		panel.add(lblUser);
+		
+		tfUser = new JTextField();
+		tfUser.setBounds(129, 0, 129, 36);
+		panel.add(tfUser);
+		tfUser.setColumns(10);
+		
+		JLabel lblPasswd = new JLabel("Password:");
+		lblPasswd.setBounds(10, 42, 119, 30);
+		panel.add(lblPasswd);
+		
+		tfPasswd = new JTextField();
+		tfPasswd.setBounds(129, 42, 129, 30);
+		panel.add(tfPasswd);
+		tfPasswd.setColumns(10);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(258, 0, 176, 72);
+		panel.add(panel_1);
+		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JButton btnLogin = new JButton("Login");
+		panel_1.add(btnLogin);
+		
+		JButton btnRegister = new JButton("Register");
+		panel_1.add(btnRegister);
+	}
+}
