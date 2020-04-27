41a74dd2a2e8f687fd2c26234e0220cd42052f3e
Orue11 <33113655+Orue11@users.noreply.github.com>
2020-04-27 09:00:25 +0200

    more languages

--- Netflix/src/main/java/es/deusto/BSPQ20_E2/Netflix/client/Internationalization.java
+++ Netflix/src/main/java/es/deusto/BSPQ20_E2/Netflix/client/Internationalization.java
@@ -5,7 +5,7 @@ import java.util.ResourceBundle;
 
 public class Internationalization {
 	
-	public static String[] Idiomas = { "en", "es", "eus" };
+	public static String[] Idiomas = { "en", "es", "eus", "fr", "de" };
 	public static ResourceBundle resourceBundle;
 	public static URLClassLoader loader;
 
